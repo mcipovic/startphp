4c187d16a8163c29a10c7ab5a0cb9f399a9ba99b
Marko <mcipovic@gmail.com>
2020-06-03 13:44:59 +0200

    Variables test

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$greeting = 'Hello';
+$name = ' Marko Cipovic';
+
+
+echo $greeting . ' ' . $name;
+echo "\n";
+
+$name2 = ' Darko';
+
+$msg2 = 'Evo vidis da znam ovo';
+
+echo $msg2 . ' ' . $name2;
+
+
+
+
+
+
